178dd78a87b7a10e45ac52fe494ac1a5e193c126
Paul ARNAUD <paul.a@sencrop.com>
2024-05-29 22:41:35 +0200

    chore: organize red or black

--- red_or_black/src/game.py
+++ red_or_black/src/game.py
@@ -17,28 +17,24 @@ class Game:
 
     def play(self) -> None:
         Logger.print_game_info("And the game begin !")
-        for i, round in enumerate(self.rounds):
-            Logger.print_game_info(f"----- Round: {round.title} -----")
+        for i, rnd in enumerate(self.rounds):
+            Logger.print_game_info(f"----- Round: {rnd.title} -----")
             for player in self.players:
                 Logger.print_game_question(f"Player {player.name}, it's your turn")
                 drawn_card = self.deck.draw()
-                answer = input(round.question)
+                answer = input(rnd.question)
 
                 player.set_round_card(i, drawn_card)
                 Logger.print_game_info(f"You draw a {drawn_card}")
-                answer_is_correct = round.validation(player, answer)
+                answer_is_correct = rnd.validation(player, answer)
                 if answer_is_correct:
-                    Logger.print_game_order(
-                        f"You can send {round.drink_quantity} drink"
-                    )
+                    Logger.print_game_order(f"You can send {rnd.drink_quantity} drink")
                     players_selection = "\n".join(
                         [f"{i}) {player.name}" for i, player in enumerate(self.players)]
                     )
                     player_number = input(f"Choose a player !\n{players_selection}\n")
-                    self.players[int(player_number)].drink(
-                        quantity=round.drink_quantity
-                    )
+                    self.players[int(player_number)].drink(quantity=rnd.drink_quantity)
                 else:
-                    Logger.print_game_order(f"Drink {round.drink_quantity}")
-                    player.drink(quantity=round.drink_quantity)
-            print(f"----- End of the round {round.title} -----")
+                    Logger.print_game_order(f"Drink {rnd.drink_quantity}")
+                    player.drink(quantity=rnd.drink_quantity)
+            print(f"----- End of the round {rnd.title} -----")
